852f5f42cb9acde3a8ac5a50f5b086700dd80444
syy616 <shiyuanyuan0616@gmail.com>
2024-08-01 20:20:13 +0800

    2024-8-1:13分提交推送了chapter4.java文件

---
+++ chapter4.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class chapter4 {
+    public static void main(String[] args) {
+        int n1 =(int)1.9;
+        System.out.println(n1);
+        int n2 =2000;
+        byte n3 = (byte)n2;
+        System.out.println(n3);
+    }
+}
